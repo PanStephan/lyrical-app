e3136faecc0c523d15df68a3f899df1a596dc26e
Stephan <stephan.startsev@gmail.com>
2020-08-10 13:57:41 +0300

    init

---
+++ webpack.dev.config.js
@@ -0,0 +1,56 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+const path = require('path')
+
+module.exports = {
+  entry: './client/index.js',
+  watchOptions: {
+    ignored: /node_modules/
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.(sa|sc|c)ss$/, 
+        use: [
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]',
+          outputPath: 'fonts/'
+        }
+      },
+      {
+        test: /\.(gif|png|jpe?g|svg)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: 'images/[name].[ext]',
+            }
+          },
+        ],
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react','@babel/preset-env']
+          }
+        }
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ filename: `index.html`, template: 'client/views/index.html'})
+  ]
+}
